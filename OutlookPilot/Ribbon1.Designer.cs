829e83e0b18b6e98d05e321dcb2733cd41986835
cilynx <randall.will@gmail.com>
2015-03-19 16:27:21 -0700

    Minimum Viable Product

---
+++ OutlookPilot/Ribbon1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace OutlookPilot
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.button7 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.button8 = this.Factory.CreateRibbonButton();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.button6 = this.Factory.CreateRibbonButton();
+            this.button9 = this.Factory.CreateRibbonButton();
+            this.box1 = this.Factory.CreateRibbonBox();
+            this.Today = this.Factory.CreateRibbonButton();
+            this.EOW = this.Factory.CreateRibbonButton();
+            this.Whenever = this.Factory.CreateRibbonButton();
+            this.box2 = this.Factory.CreateRibbonBox();
+            this.Tomorrow = this.Factory.CreateRibbonButton();
+            this.PickDate = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.box1.SuspendLayout();
+            this.box2.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabMail";
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabMail";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button4);
+            this.group1.Items.Add(this.button7);
+            this.group1.Items.Add(this.button2);
+            this.group1.Items.Add(this.button5);
+            this.group1.Items.Add(this.button8);
+            this.group1.Items.Add(this.button3);
+            this.group1.Items.Add(this.button6);
+            this.group1.Items.Add(this.button9);
+            this.group1.Items.Add(this.box1);
+            this.group1.Items.Add(this.box2);
+            this.group1.Label = "Defer";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.KeyTip = "CMA";
+            this.button1.Label = "1";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Label = "4";
+            this.button4.Name = "button4";
+            this.button4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
+            // 
+            // button7
+            // 
+            this.button7.Label = "7";
+            this.button7.Name = "button7";
+            this.button7.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button7_Click);
+            // 
+            // button2
+            // 
+            this.button2.Label = "2";
+            this.button2.Name = "button2";
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Label = "5";
+            this.button5.Name = "button5";
+            this.button5.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button5_Click);
+            // 
+            // button8
+            // 
+            this.button8.Label = "8";
+            this.button8.Name = "button8";
+            this.button8.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button8_Click);
+            // 
+            // button3
+            // 
+            this.button3.Label = "3";
+            this.button3.Name = "button3";
+            this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            this.button6.Label = "6";
+            this.button6.Name = "button6";
+            this.button6.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button6_Click);
+            // 
+            // button9
+            // 
+            this.button9.KeyTip = "9";
+            this.button9.Label = "9";
+            this.button9.Name = "button9";
+            this.button9.ScreenTip = "Defer 9 Days";
+            this.button9.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button9_Click);
+            // 
+            // box1
+            // 
+            this.box1.BoxStyle = Microsoft.Office.Tools.Ribbon.RibbonBoxStyle.Vertical;
+            this.box1.Items.Add(this.Today);
+            this.box1.Items.Add(this.Tomorrow);
+            this.box1.Items.Add(this.EOW);
+            this.box1.Name = "box1";
+            // 
+            // Today
+            // 
+            this.Today.Label = "Today";
+            this.Today.Name = "Today";
+            this.Today.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.today_Click);
+            // 
+            // EOW
+            // 
+            this.EOW.Label = "This Week";
+            this.EOW.Name = "EOW";
+            this.EOW.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.eow_Click);
+            // 
+            // Whenever
+            // 
+            this.Whenever.Label = "Whenever";
+            this.Whenever.Name = "Whenever";
+            this.Whenever.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Whenever_Click);
+            // 
+            // box2
+            // 
+            this.box2.BoxStyle = Microsoft.Office.Tools.Ribbon.RibbonBoxStyle.Vertical;
+            this.box2.Items.Add(this.PickDate);
+            this.box2.Items.Add(this.Whenever);
+            this.box2.Name = "box2";
+            // 
+            // Tomorrow
+            // 
+            this.Tomorrow.Label = "Tomorrow";
+            this.Tomorrow.Name = "Tomorrow";
+            this.Tomorrow.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.tomorrow_Click);
+            // 
+            // PickDate
+            // 
+            this.PickDate.Label = "Pick Date";
+            this.PickDate.Name = "PickDate";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.box1.ResumeLayout(false);
+            this.box1.PerformLayout();
+            this.box2.ResumeLayout(false);
+            this.box2.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button6;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button7;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button8;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button9;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton Today;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton Tomorrow;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton EOW;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton Whenever;
+        internal Microsoft.Office.Tools.Ribbon.RibbonBox box1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonBox box2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton PickDate;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ OutlookPilot/Ribbon1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace OutlookPilot
+{
+    using Outlook = Microsoft.Office.Interop.Outlook;
+    using System.Diagnostics; // For Debug.writeLine
+    using System.Windows.Forms; // For MessageBox
+    
+    public partial class Ribbon1
+    {
+       
+        /* TODO:
+         * Weekend Detection / Warning / Rescheduling
+         * Busy Day Detection / Warning / Rescheduling
+         * Calendar-based Busy Calculations
+         */
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e) 
+        {
+            removeEmptyFolders();
+        }
+
+        private void removeEmptyFolders()
+        {
+            /* TODO: Make this configurable */
+            Outlook.MAPIFolder pilotFolder = Globals.ThisAddIn.Application.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox).Folders["Pilot"];
+            
+            /* Remove any empty Pilot folders */
+            for (int i=1; i <= pilotFolder.Folders.Count; i++)
+            {
+                if(pilotFolder.Folders[i].Items.Count == 0)
+                {
+                    Debug.WriteLine(pilotFolder.Folders[i].Name + " is empty and we are deleting it");
+                    pilotFolder.Folders[i].Delete();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(1)); }
+        private void button2_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(2)); }
+        private void button3_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(3)); }
+        private void button4_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(4)); }
+        private void button5_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(5)); }
+        private void button6_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(6)); }
+        private void button7_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(7)); }
+        private void button8_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(8)); }
+        private void button9_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(9)); }
+        private void today_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now); }
+        private void tomorrow_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(1)); }
+        private void Whenever_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Now.AddDays(-1)); }
+
+
+        /* TODO: Find the least busy day this week and make it happen */
+        private void eow_Click(object sender, RibbonControlEventArgs e) { defer(DateTime.Today);  }
+       
+        private void defer(DateTime date)
+        {
+            /* Dialog Results */
+            DialogResult dr;
+            /* Prompt for Busy? */
+            bool promptBusy = true;
+            /* Prompt for Weekend? */
+            bool promptWeekend = true;
+
+            /* "Whenever" passes yesterday.  Start scheduling options with tomorrow. */
+            if(date < DateTime.Today)
+            {
+                date = DateTime.Today.AddDays(1);
+                promptBusy = false;
+                promptWeekend = false;
+            }
+
+            /* If we're deferring to a weekend, let's make sure that's what we want */
+            if(date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                dr = DialogResult.Yes;
+                if (promptWeekend)
+                {
+                    dr = MessageBox.Show("The day you selected is a weekend.  Would you like to reschedule for the next available week day?", "Weekend Work?", MessageBoxButtons.YesNo);
+                }
+                if(dr == DialogResult.Yes)
+                {
+                    promptBusy = false;                      
+                    while(date.DayOfWeek != DayOfWeek.Monday)
+                    {
+                        date = date.AddDays(1);
+                    }
+                    Debug.WriteLine("Pushing out to Monday: " + date.ToString("yyyyMMdd"));
+                }
+            }
+
+            /* Define the main Pilot folder
+             * TODO: Make this configurable */
+            Outlook.MAPIFolder pilotFolder = Globals.ThisAddIn.Application.Session.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox).Folders["Pilot"];            
+
+            /* I like this format better than MailPilot's, but it means I should implement a MailPilot compatibility mode 
+             * such that MailPilot and OutlookPilot can be used on the same set of folders and play nice together */
+            String folderName = date.ToString("yyyyMMdd");
+
+            /* Create our targetFolder if it does not already exist then grab the object */
+            try { pilotFolder.Folders.Add(folderName); }
+            catch { Debug.WriteLine("Folder already exists: " + folderName); }
+            Outlook.MAPIFolder targetFolder = pilotFolder.Folders[folderName];
+
+            /* If we're already really busy on the target day, see if we want to push things out
+             * TODO: Make this configurable */
+            while(targetFolder.Items.Count >= 5)
+            {
+                dr = DialogResult.Yes; // Set to yes so we reschedule by default if the prompt is disabled
+                if(promptBusy)
+                {
+                    dr = MessageBox.Show("You're already pretty busy on " + folderName + ". Would you like to reschedule for your new available day?", "Getting Busy!", MessageBoxButtons.YesNo);
+                }
+                if (dr == DialogResult.Yes) // We want to reschedule
+                {
+                    promptBusy = false;
+                    date = date.AddDays(1);
+                    if (date.DayOfWeek == DayOfWeek.Saturday) { date = date.AddDays(2); } // Don't automagically schedule things on weekends
+                    folderName = date.ToString("yyyyMMdd");
+                    try { pilotFolder.Folders.Add(folderName); }
+                    catch { Debug.WriteLine("Folder already exists: " + folderName); }
+                    targetFolder = pilotFolder.Folders[folderName];
+                }
+                else // We do not want to reschedule
+                {
+                    break;
+                }
+            }
+
+            /* Get our Selected message / conversation */
+            Outlook.Selection selection = Globals.ThisAddIn.Application.ActiveExplorer().Selection;
+            Debug.WriteLine("Selection.Count = " + selection.Count);
+            Outlook.Selection convHeaders = selection.GetSelection(Outlook.OlSelectionContents.olConversationHeaders) as Outlook.Selection;
+            Debug.WriteLine("Selection.Count (ConversationHeaders) = " + convHeaders.Count);
+
+            /* If we have a Conversation selected.  A Conversation is either the collapsed header 
+             * of a conversation or a single message that is not part of a conversation */
+            if (convHeaders.Count >= 1)
+            {
+                foreach (Outlook.ConversationHeader convHeader in convHeaders)
+                {
+                    Outlook.SimpleItems items = convHeader.GetItems();
+                    for (int i = 1; i <= items.Count; i++)
+                    {
+                        Outlook.MailItem mail = items[i] as Outlook.MailItem;
+                        mail.UnRead = false;
+                      
+                        if (mail.Parent.FolderPath != targetFolder.FolderPath)
+                        {
+                            Debug.WriteLine("Moving " + mail.Subject + " to " + targetFolder.FolderPath);
+                            mail.Move(targetFolder);
+                        }
+                        else
+                        {
+                            Debug.WriteLine(mail.Subject + " is already in " + targetFolder.FolderPath + ".  Doing nothing.");
+                        }
+                    }
+                }
+            }
+            else // Otherwise, we have selected a single message that is part of a larger Conversation.
+            {
+                Outlook.MailItem mail = selection[1] as Outlook.MailItem;
+                mail.UnRead = false;
+                Debug.WriteLine("Moving " + mail.Subject + " to " + targetFolder.FolderPath);
+                if(mail.Parent != targetFolder.Name)
+                {
+                    mail.Move(targetFolder);
+                }
+                else
+                {
+                    Debug.WriteLine(mail.Subject + " is already in " + targetFolder.FolderPath + ".  Doing nothing.");
+                }
+            }
+
+            removeEmptyFolders();
+        }
+
+        private void NextWeek_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+
+       
+    }
+}
